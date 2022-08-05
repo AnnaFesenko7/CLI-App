b78779c6864f1455c3ca3fae4fac2015c52d08ef
AnnaFesenko7 <annafecenko7@gmail.com>
2022-08-05 13:10:55 +0200

    add files

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # CLI-App
+
+Link to screenshots:
+
+https://monosnap.com/list/62ecf8a78a2e601ddf11dd1e
---
+++ index.js
@@ -0,0 +1,33 @@
+const contactOperations = require("./contacts.js");
+const argv = require("yargs").argv;
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const contacts = await contactOperations.listContacts();
+      console.log(contacts);
+      break;
+    case "get":
+      const searchedContact = await contactOperations.getContactById(id);
+      if (!searchedContact) {
+        throw new Error(`Contact with id: ${id} not found`);
+      }
+      console.log(searchedContact);
+      break;
+    case "add":
+      const newContact = await contactOperations.addContact(name, email, phone);
+      console.log(newContact);
+      break;
+    case "remove":
+      const removedContact = await contactOperations.removeContact(id);
+      if (!removedContact) {
+        throw new Error(`Contact with id: ${id} not found`);
+      }
+      console.log(removedContact);
+      break;
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+invokeAction(argv);
